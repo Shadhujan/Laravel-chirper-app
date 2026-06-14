81c5bae7bed230dcbbc41a3a802d88998d81709b
shadhujan <jeya38shadhujan@gmail.com>
2026-06-15 02:52:37 +0530

    health rounte endpoint

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/hello', function() {
     return ("Hello Shadhujan, Laravel is working!!!");
 });
+
+Route::get('/health-v1', function() {
+    return "up and running " . now();
+});
+
+//json parse
+Route::get('/health-v2', function() {
+    return response()->json([
+        'status' => 'up and running',
+        'time' => now()
+    ]);
+});
