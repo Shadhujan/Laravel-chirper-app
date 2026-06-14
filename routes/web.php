c3a9f0552d6700839a37ba2d4414988c14350073
shadhujan <jeya38shadhujan@gmail.com>
2026-06-15 03:05:04 +0530

    create the controller, update the route to point that contreller class to that index function

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
 
 Route::get('/hello', function() {
     return ("Hello Shadhujan, Laravel is working!!!");
